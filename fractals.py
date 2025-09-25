1fd2ddcacc7f0231c0140a5ee170ed65f44bd1f7
Layan Jethwa <lrsjethwa3.14@gmail.com>
2025-09-25 14:31:58 +0100

    added pan and zoom

--- fractals.py
+++ fractals.py
@@ -30,9 +30,9 @@ def mandelbrot_basic(point, max = iterations, start = 0):
         start = (start**2) + point
     return max
 
-def mandelbrot(bounds, max = iterations, start = 0):
-    re = np.linspace(bounds[0], bounds[1], 800)
-    im = np.linspace(bounds[0], bounds[1], 800)
+def mandelbrot(bounds, max = iterations):
+    re = np.linspace(bounds[0][0], bounds[0][1], 800)
+    im = np.linspace(bounds[1][0], bounds[1][1], 800)
     x, y = np.meshgrid(re, im)
     points = x + 1j*y
 
@@ -48,17 +48,23 @@ def mandelbrot(bounds, max = iterations, start = 0):
     return M
 
 
-bounds = (-2,2)
+bounds = ((-2,2),(-2,2))
+
+def compute_size(bounds):
+    return bounds[0][1]-bounds[0][0]
+
+size = compute_size(bounds)
+
 def coordinate(x,y):
-    return ((x+(0-bounds[0]))*(800/(bounds[1]-bounds[0])), 800-((y+(0-bounds[0]))*(800/(bounds[1]-bounds[0]))))
+    return ((x+(0-bounds[0][0]))*(800/size), 800-((y+(0-bounds[1][0]))*(800/size)))
 
 def basic_compute():
     samples = 800
     surface_array = np.zeros((800, 800, 3))
-    factor = samples/(bounds[1]-bounds[0])
-    for x in range(int(bounds[0]*factor),int(bounds[1]*factor)):
+    factor = samples/size
+    for x in range(int(bounds[0][0]*factor),int(bounds[0][1]*factor)):
         x = x/factor
-        for y in range(int(bounds[0]*factor),int(bounds[1]*factor)):
+        for y in range(int(bounds[1][0]*factor),int(bounds[1][1]*factor)):
             y = y/factor
             coordinates = tuple(int(i) for i in coordinate(x,y))
             try: surface_array[coordinates] = colours[mandelbrot_basic(complex(x,0)+complex(0,y))]
@@ -66,12 +72,18 @@ def basic_compute():
     fractal_surface = pygame.surfarray.make_surface(surface_array)
     return fractal_surface
 
-#fractal_surface = basic_compute()
+def vectorisation():
+    M = mandelbrot(bounds)
+    surface_array = colours[M]
 
-M = mandelbrot(bounds)
-surface_array = colours[M]
+    return pygame.surfarray.make_surface(np.swapaxes(surface_array, 0, 1))
 
-fractal_surface = pygame.surfarray.make_surface(np.swapaxes(surface_array, 0, 1))
+def calculate():
+    #fractal_surface = basic_compute()
+    fractal_surface = vectorisation()
+    return fractal_surface
+
+fractal_surface = calculate()
 
 while running:
     for event in pygame.event.get(): 
@@ -81,5 +93,28 @@ while running:
             sys.exit()
             exit()
             quit()
+
+        elif event.type == pygame.MOUSEWHEEL:
+            print(bounds)
+            if event.y == 1:
+                bounds = ((bounds[0][0]+size/4,bounds[0][1]-size/4),(bounds[1][0]+size/4,bounds[1][1]-size/4))
+            elif event.y == -1:
+                bounds = ((bounds[0][0]-size/2,bounds[0][1]+size/2),(bounds[1][0]-size/2,bounds[1][1]+size/2))
+            size = compute_size(bounds)
+            print(bounds)
+            fractal_surface = calculate()
+
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                bounds = (tuple(i-size*0.1 for i in bounds[0]), bounds[1])
+            elif event.key == pygame.K_RIGHT:
+                bounds = (tuple(i+size*0.1 for i in bounds[0]), bounds[1])
+            elif event.key == pygame.K_UP:
+                bounds = (bounds[0], tuple(i-size*0.1 for i in bounds[1]))
+            elif event.key == pygame.K_DOWN:
+                bounds = (bounds[0], tuple(i+size*0.1 for i in bounds[1]))
+            size = compute_size(bounds)
+            fractal_surface = calculate()
+
     screen.blit(fractal_surface, (0, 0))
     pygame.display.update()
